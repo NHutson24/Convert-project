7f34634ee543125139c8b01f9edbe71138124167
Nkosi Hutson <rashaanhutson@gmail.com>
2025-12-03 13:12:38 -0400

    update js for distance conversion

--- ConvertApp.js
+++ ConvertApp.js
@@ -10,3 +10,25 @@ categorySelect.addEventListener("change", function() {
 
     document.getElementById(userInput).style.display = 'block';
 });
+
+document.getElementById("distanceConvertBtn").addEventListener("click", disfFn);
+document.getElementById("temperatureConvertBtn").addEventListener("click", tempFn);
+document.getElementById("currencyCOnvertBtn").addEventListener("click", currencyFn);
+
+
+function disfFn(){
+    let value = parseFloat(document.getElementById("distanceInput").value);
+    let fromUnit = document.getElementById("fromDistanceUnit").value;
+    let toUnit = document.getElementById("toDistanceUnit").value;
+
+    let result = 0;
+
+    if(fromUnit === "miles" && toUnit === "kilometers"){
+        result = (value) * 1.60934;
+    }
+    else if(fromUnit === "kilometers" && toUnit === "miles"){
+        result = (value) * 0.621371;
+    }
+
+    document.getElementById("distanceResult").textContent = `Result: ${result.toFixed(2)} ${toUnit}`;
+}
